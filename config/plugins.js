88b6edfa6b654f1c147db9ade57fa451cab9e533
abdullahzahid321456 <abdullahzahid321456@gmail.com>
2024-09-09 15:01:05 +0500

    updated

--- config/plugins.js
+++ config/plugins.js
@@ -1,19 +1,12 @@
 module.exports = ({ env }) => ({
   upload: {
-    config: {
-      provider: 'aws-s3',
-      providerOptions: {
-        accessKeyId: env('AWS_ACCESS_KEY'),
-        secretAccessKey: env('AWS_SECRET_KEY'),
-        region: env('AWS_REGION'),
-        params: {
-          Bucket: env('S3_BUCKET_NAME'),
-        },
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
+    provider: 'aws-s3',
+    providerOptions: {
+      accessKeyId: env('AWS_ACCESS_KEY'),
+      secretAccessKey: env('AWS_SECRET_KEY'),
+      region: env('AWS_REGION'),
+      params: {
+        Bucket: env('S3_BUCKET_NAME'),
       },
     },
   },
